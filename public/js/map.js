550a6b9fd8e8be8666aba4907df8410f1ca367d0
RishabhGit001 <rishabhthapliyal001.2@gmail.com>
2024-03-01 20:14:49 +0530

    first commit

---
+++ public/js/map.js
@@ -0,0 +1,17 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: listing.geometry.coordinates, // [lng, lat]
+  zoom: 11,
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .addTo(map)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h5>${listing.title}</h5><p>Exact location provided after booking</p>`
+    )
+  );
